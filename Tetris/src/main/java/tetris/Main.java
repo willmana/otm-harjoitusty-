2e5acdcea0a8968dfe2494f7fdf4ee95a36c01e5
willmana <aleksi.willman@gmail.com>
2018-04-11 15:44:58 +0300

    main

---
+++ Tetris/src/main/java/tetris/Main.java
@@ -0,0 +1,12 @@
+
+package tetris;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Tetris tetris = new Tetris();
+        tetris.setLocationRelativeTo(null); //center
+        tetris.setVisible(true);
+    }
+}
--- Tetris/src/main/java/tetris/Tetris.java
+++ Tetris/src/main/java/tetris/Tetris.java
@@ -25,9 +25,5 @@ public class Tetris extends JFrame{
         return statusBar;
     }
     
-    public static void main(String[] args) {
-        Tetris tetris = new Tetris();
-        tetris.setLocationRelativeTo(null); //center
-        tetris.setVisible(true);
-    }
+
 }
